52c518cd880fd4ce7637205df5be12ec0e5853c4
Marina_Panteleeva <marinawork1989@gmail.com>
2020-02-28 12:58:22 +0300

    добавление группы тестов для удаления

--- addressbook-web-tests/src/test/java/sqta/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/sqta/GroupCreationTests.java
@@ -1,37 +1,9 @@
 package sqta;
 
-import java.util.concurrent.TimeUnit;
-
 import org.testng.annotations.*;
 
-import static org.testng.Assert.*;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-public class GroupCreationTests {
-    private FirefoxDriver driver;
-    private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
-
-    @BeforeClass(alwaysRun = true)
-    public void setUp() throws Exception {
-        System.setProperty("webdriver.gecko.driver", "C:\\SeleniumGecko\\geckodriver.exe");
-        driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        driver.get("http://localhost/addressbook/group.php");
-        login("admin", "secret");
-    }
-
-    private void login(String username, String password) {
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).sendKeys(username);
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys(password);
-        driver.findElement(By.xpath("//input[@value='Login']")).click();
-    }
+public class GroupCreationTests extends TestBase{
 
     @Test
     public void testGroupCreation() throws Exception {
@@ -39,76 +11,7 @@ public class GroupCreationTests {
       initGroupCreation();
       fillGroupCreation(new GroupData("test2", "test3", "test4"));
       submitGroupCreation();
-      returnToGroupCreation();
+      returnToGroupPage();
     }
 
-  private void returnToGroupCreation() {
-    driver.findElement(By.linkText("group page")).click();
-  }
-
-  private void submitGroupCreation() {
-    driver.findElement(By.name("submit")).click();
-  }
-
-  private void fillGroupCreation(GroupData groupData) {
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    driver.findElement(By.name("group_footer")).click();
-    driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  private void initGroupCreation() {
-    driver.findElement(By.name("new")).click();
-  }
-
-  private void gotoGroupPage() {
-    driver.findElement(By.linkText("groups")).click();
-  }
-
-  @AfterClass(alwaysRun = true)
-    public void tearDown() throws Exception {
-        driver.close();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
-        }
-    }
 }
