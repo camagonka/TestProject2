41d245c29c438f880e703004a2a99d76393201fe
Marina_Panteleeva <marinawork1989@gmail.com>
2020-03-19 14:47:24 +0300

    использование компоратора для выявления максимального id

--- addressbook-web-tests/src/test/java/sqta/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/sqta/tests/GroupCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import sqta.model.GroupData;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -13,8 +15,19 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().createGroup(new GroupData("test6", null, null));
+        GroupData group = new GroupData("test7", null, null);
+        app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
+        int max = 0;
+        for (GroupData g : after) {
+            if (g.getId() > max) {
+                max = g.getId();
+            }
+        }
+
+        group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
