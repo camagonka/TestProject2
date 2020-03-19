5885b61b7e3b4e6c6fc9cfa4ddef3ce527f623a9
Marina_Panteleeva <marinawork1989@gmail.com>
2020-03-19 14:59:00 +0300

    сортировка списка

--- addressbook-web-tests/src/test/java/sqta/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/sqta/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import sqta.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -28,6 +29,9 @@ public class GroupModificationTests extends TestBase{
         before.remove(before.size()-1);
         before.add(group);
 
+        Comparator<? super GroupData> byId = Comparator.comparingInt(GroupData::getId);
+        before.sort(byId);
+        after.sort(byId);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
