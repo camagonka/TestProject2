8b4df1f187fadc6c75afd4e85d8cc56390f42a80
Marina_Panteleeva <marinawork1989@gmail.com>
2020-04-06 14:50:44 +0300

    изменение настроек в II. дебаг и запуск тестов с помощью TestNG

--- addressbook-web-tests/src/test/java/sqta/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/sqta/tests/TestBase.java
@@ -6,7 +6,7 @@ import sqta.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeSuite
     public void setUp() throws Exception {
